0d9f03d4a98cc8751cb0abc9d45edb065595d967
gembkkim <gembkkim@gmail.com>
2024-07-12 14:04:51 +0900

    1122

--- 소스.cpp
+++ 소스.cpp
@@ -12,7 +12,7 @@
 //askjdjasdhjkashdkj
 //askjdjasdhjkashdkj
 //askjdjasdhjkashdkj
-//askjdjasdhjkashdkj
+//askjdjasdhjkashdkj분기를 원격 리포지토리에 푸시하지 못했습니다. 자세한 내용은 출력 창을 참조하세요. 
 //askjdjasdhjkashdkj
 //askjdjasdhjkashdkj
 //askjdjasdhjkashdkj
